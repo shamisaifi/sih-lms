008a32003ce692ab32f9e92186820ab3995b5d89
Devender Pal <57867128+devender15@users.noreply.github.com>
2024-08-31 21:03:37 +0530

    feat: Add Hero, Login, and Register pages
    This commit adds three new pages: Hero, Login, and Register. The Hero page displays a welcome message, while the Login and Register pages provide forms for users to sign in and create an account. These pages are part of the authentication flow in the application.

---
+++ client/src/pages/hero.jsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+    return (
+        <div className="flex h-screen w-screen justify-center items-center">
+            <h1>
+                Welcome to GyaanSetu
+            </h1>
+        </div>
+    )
+}
