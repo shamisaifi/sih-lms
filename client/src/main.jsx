ee61fe5ebf48f53d89288634527c1e3d9ac04d01
Devender Pal <57867128+devender15@users.noreply.github.com>
2024-09-01 14:10:20 +0530

    chore: Refactor App and Portal components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,20 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-<<<<<<< HEAD
 import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom'
-=======
-import { BrowserRouter } from 'react-router-dom'
->>>>>>> ujala
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-<<<<<<< HEAD
     <App />
-=======
-      <App />
->>>>>>> ujala
     </BrowserRouter>
   </StrictMode>,
 )
