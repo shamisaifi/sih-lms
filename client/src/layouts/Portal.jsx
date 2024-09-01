57c70c4d77c2dc4c617e508ff21ada8629af43d2
ujala <singhujala2004@gmail.com>
2024-09-01 16:27:40 +0530

    added navbar

--- client/src/layouts/Portal.jsx
+++ client/src/layouts/Portal.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import Sidebar from "../components/ui/Sidebar";
 import { Routes, Route } from "react-router-dom";
+import Navbar from "@/components/Navbar";
 
 const PortalLayout = () => {
   return (
-    <div className="flex items-start w-full h-full">
-      <Sidebar />
-      <main className="w-full h-full">
-        <Routes>
-          <Route path="/" element={<h1>Dashboard</h1>} />
-          <Route path="courses" element={<h1>Users</h1>} />
-          <Route path="saved" element={<h1>Products</h1>} />
-          <Route path="orders" element={<h1>Orders</h1>} />
-        </Routes>
-      </main>
-    </div>
+    <>
+      <Navbar/>
+      <div className="flex items-start w-full h-full">
+        <Sidebar />
+        <main className="w-full h-full">
+          <Routes>
+            <Route path="/" element={<h1>Dashboard</h1>} />
+            <Route path="courses" element={<h1>Users</h1>} />
+            <Route path="saved" element={<h1>Products</h1>} />
+            <Route path="orders" element={<h1>Orders</h1>} />
+          </Routes>
+        </main>
+      </div>
+    </>
   );
 };
 
