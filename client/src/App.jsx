9fdc8f59dd3c97dbbe9fc0c76274537d4ecf8495
Devender Pal <57867128+devender15@users.noreply.github.com>
2024-09-01 14:08:48 +0530

    chore: Refactor App and Portal components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "./components/ui/sonner";
 
@@ -8,13 +7,8 @@ import { UserProvider } from "./providers/auth-provider";
 
 import { Hero, Login, Register } from "./pages";
 
-=======
-import Navbar from "./components/Navbar"
-import { Button } from "./components/ui/button"
->>>>>>> ujala
 function App() {
   return (
-<<<<<<< HEAD
     <UserProvider>
       <Routes>
         <Route element={<Hero />} exact path="/" />
@@ -27,14 +21,6 @@ function App() {
       <Toaster />
     </UserProvider>
   );
-=======
-    <>
-      {/* <Button variant="ghost" /> */}
-      <Navbar />
-      
-    </>
-  )
->>>>>>> ujala
 }
 
 export default App;
