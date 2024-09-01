b33bd026f3aa0c6f0ca15d1c696eb323f8ed990d
shamisaifi <shamisaifi2003@gmail.com>
2024-09-01 19:36:28 +0530

    minor changes

--- client/src/components/ui/Sidebar.jsx
+++ client/src/components/ui/Sidebar.jsx
@@ -26,10 +26,10 @@ function Sidebar() {
 
   return (
     <div
-      className={`relative flex items-start flex-col h-screen bg-gray-800 py-2 px-4 text-white transition-all duration-300 ease-in-out `}
+      className={`relative flex items-start flex-col h-screen bg-gray-800 py-2 px-4 text-white transition-all duration-250 ease `}
       style={{ width: isOpen ? "18rem" : "4.5rem" }}
     >
-      <div className="w-full overflow-hidden flex justify-end mb-8">
+      <div className="w-full overflow-hidden flex justify-end ">
         <div className="cursor-pointer" onClick={toggleSidebar}>
           {isOpen ? (
             <BiMenuAltRight className="w-7 h-7" />
