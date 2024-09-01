40f77b641549903ccdba20a5068bf6646fb29cfc
Devender Pal <57867128+devender15@users.noreply.github.com>
2024-09-01 16:22:18 +0530

    chore: Refactor Portal layout and add routes for dashboard, users, products, and orders

--- client/src/layouts/Portal.jsx
+++ client/src/layouts/Portal.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import Sidebar from "../components/ui/Sidebar"
-import { Outlet } from "react-router-dom";
+import Sidebar from "../components/ui/Sidebar";
+import { Routes, Route } from "react-router-dom";
 
 const PortalLayout = () => {
   return (
-    <>
+    <div className="flex items-start w-full h-full">
       <Sidebar />
-      <h1>portal</h1>
-    </>
+      <main className="w-full h-full">
+        <Routes>
+          <Route path="/" element={<h1>Dashboard</h1>} />
+          <Route path="courses" element={<h1>Users</h1>} />
+          <Route path="saved" element={<h1>Products</h1>} />
+          <Route path="orders" element={<h1>Orders</h1>} />
+        </Routes>
+      </main>
+    </div>
   );
 };
 
